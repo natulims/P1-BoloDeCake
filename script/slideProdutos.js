2b79ae7e57657367c08ba701bb9b1c81272770e2
plfmoura <plfmoura96@gmail.com>
2022-09-29 21:18:03 -0300

    clone para branch, verificar parar merge produtos

---
+++ script/slideProdutos.js
@@ -0,0 +1,32 @@
+var swiper = new Swiper(".slide-content-bolos", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".slide-content-doces", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination-doce",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next-doce",
+      prevEl: ".swiper-button-prev-doce",
+    },
+  });
+
