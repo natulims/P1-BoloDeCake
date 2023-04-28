934821ab6f164159d2aea8c595f8b9eb53b2a2a5
Natalia Carvalho <ssspinosaure@gmail.com>
2023-04-28 18:55:17 -0300

    adicionando categorias de bolo

--- my-project/src/pages/products/Cakes.jsx
+++ my-project/src/pages/products/Cakes.jsx
@@ -1,9 +1,421 @@
-import React from 'react'
+import React from "react";
 
 function Cakes() {
   return (
-    <div>Cakes</div>
-  )
+    <>
+      <section>
+        {" "}
+        <h1 className="text-center flex flex-row justify-center gap-6 text-midnightblue capitalize text-3xl lg:text-md">
+          <span>
+            <svg
+              preserveAspectRatio="none"
+              data-bbox="0 0 323.295 323.296"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 323.295 323.296"
+              role="presentation"
+              aria-hidden="true"
+              fill="#FE765E"
+            >
+              <g>
+                <path d="M161.647 0l57.151 104.497 104.497 57.15-104.497 57.152-57.151 104.497-57.15-104.497L0 161.647l104.497-57.15L161.647 0z"></path>
+              </g>
+            </svg>
+          </span>
+          Bolos Caseiros
+          <span>
+            <svg
+              preserveAspectRatio="none"
+              data-bbox="0 0 323.295 323.296"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 323.295 323.296"
+              role="presentation"
+              aria-hidden="true"
+              fill="#FE765E"
+            >
+              <g>
+                <path d="M161.647 0l57.151 104.497 104.497 57.15-104.497 57.152-57.151 104.497-57.15-104.497L0 161.647l104.497-57.15L161.647 0z"></path>
+              </g>
+            </svg>
+          </span>
+        </h1>
+        <article className="grid grid-cols-1 lg:grid-cols-4 gap-5">
+          <div className="ml-8 lg:ml-16">
+            {" "}
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            {" "}
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            {" "}
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            {" "}
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            {" "}
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-8 lg:ml-16">
+            {" "}
+            <div>
+              <div class="group my-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-300 hover:shadow-lg bg-white shadow-md">
+                <figure>
+                  <img
+                    className="rounded"
+                    src="https://www.estadao.com.br/resizer/oFDrDp3xgfze9zuyNaR5gnyURVA=/arc-anglerfish-arc2-prod-estadao/public/FIVYQFU6J5ND3PYRA6XQHR4NW4.jpg"
+                    alt=""
+                  />
+                </figure>
+                <div class="mt-4 px-5 pb-5">
+                  <a href="#">
+                    <h5 class="text-xl tracking-tighter text-midnightblue font-semibold">
+                      Bolo de brigadeiro de chocolate
+                    </h5>
+                  </a>
+                  <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                      <span className="text-darkpurple font-bold">R$69,90</span>
+                    </p>
+                  </div>
+                  <a
+                    href="#"
+                    class="flex items-center justify-center rounded-md bg-bittersweet hover:bg-cinnabar px-5 py-2.5 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-cinnabar"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="mr-2 h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    Adicionar ao carrinho
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </article>
+      </section>
+    </>
+  );
 }
 
-export default Cakes
+export default Cakes;
--- my-project/src/pages/products/Products.jsx
+++ my-project/src/pages/products/Products.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Search from "../../components/searchbar/Search";
+import ToggleCake from "../../components/toggle/ToggleCake";
 
 function Products() {
   return (
-    <div>Products</div>
-  )
+    <>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("https://images.unsplash.com/photo-1542826438-bd32f43d626f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1292&q=80")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="my-10">
+        <Search />
+        <ToggleCake/>
+      </div>
+    </>
+  );
 }
 
-export default Products
+export default Products;
