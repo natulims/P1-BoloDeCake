dcb4b6ba88cdc23a84d8aadb1f6b79bb44f01afe
Natalia Carvalho <ssspinosaure@gmail.com>
2023-04-28 00:01:06 -0300

    adicionando as rotas

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,15 +1,32 @@
 import React from "react";
-import Footer from "./components/footer/Footer"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./components/footer/Footer";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
+import Team from "./pages/team/Team";
+import Products from "./pages/products/Products";
+import Cart from "./pages/cartPage/CartPage";
 
 function App() {
   return (
-  <>
-  <Navbar/>
-  <Home/>
-  <Footer/>
-  </>
+    <>
+      <BrowserRouter>
+        <nav>
+          <Navbar />
+        </nav>
+        <Routes>
+          <Route>
+            <Route path="/" element={<Home />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/cart" element={<Cart />} />
+          </Route>
+        </Routes>
+        <footer>
+          <Footer />
+        </footer>
+      </BrowserRouter>
+    </>
   );
 }
 
