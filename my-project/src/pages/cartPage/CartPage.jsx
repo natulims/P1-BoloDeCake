c18e472541479830742629e6400c7f56c80112b3
Natalia Carvalho <ssspinosaure@gmail.com>
2023-05-09 19:34:53 -0300

    arrumando layout da pagina

--- my-project/src/pages/cartPage/CartPage.jsx
+++ my-project/src/pages/cartPage/CartPage.jsx
@@ -83,6 +83,135 @@ function CartPage() {
                   </button>
                 </div>
               </li>
+
+              <li className="flex items-center gap-4">
+                <img
+                  src="https://www.carolcalazans.com.br/wp-content/uploads/2021/02/bolo-chocolatudo-2.jpg"
+                  alt=""
+                  className="h-16 w-16 rounded object-cover"
+                />
+
+                <div>
+                  <h3 className="text-sm text-cinnabar font-bold">
+                    Bolo de chocolate
+                  </h3>
+                </div>
+
+                <div className="flex flex-1 items-center justify-end gap-2">
+                  <form>
+                    <input
+                      type="number"
+                      min="1"
+                      className="h-8 w-12 rounded focus:border focus:border-bittersweet bg-whitesmoke p-0 text-center text-xs text-midnightblue focus:text-cinnabar [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+                    />
+                  </form>
+
+                  <button className="text-midnightblue transition hover:text-cinnabar">
+                    <span className="sr-only">Remove item</span>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </li>
+
+              <li className="flex items-center gap-4">
+                <img
+                  src="https://www.carolcalazans.com.br/wp-content/uploads/2021/02/bolo-chocolatudo-2.jpg"
+                  alt=""
+                  className="h-16 w-16 rounded object-cover"
+                />
+
+                <div>
+                  <h3 className="text-sm text-cinnabar font-bold">
+                    Bolo de chocolate
+                  </h3>
+                </div>
+
+                <div className="flex flex-1 items-center justify-end gap-2">
+                  <form>
+                    <input
+                      type="number"
+                      min="1"
+                      className="h-8 w-12 rounded focus:border focus:border-bittersweet bg-whitesmoke p-0 text-center text-xs text-midnightblue focus:text-cinnabar [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+                    />
+                  </form>
+
+                  <button className="text-midnightblue transition hover:text-cinnabar">
+                    <span className="sr-only">Remove item</span>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </li>
+
+              <li className="flex items-center gap-4">
+                <img
+                  src="https://www.carolcalazans.com.br/wp-content/uploads/2021/02/bolo-chocolatudo-2.jpg"
+                  alt=""
+                  className="h-16 w-16 rounded object-cover"
+                />
+
+                <div>
+                  <h3 className="text-sm text-cinnabar font-bold">
+                    Bolo de chocolate
+                  </h3>
+                </div>
+
+                <div className="flex flex-1 items-center justify-end gap-2">
+                  <form>
+                    <input
+                      type="number"
+                      min="1"
+                      className="h-8 w-12 rounded focus:border focus:border-bittersweet bg-whitesmoke p-0 text-center text-xs text-midnightblue focus:text-cinnabar [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
+                    />
+                  </form>
+
+                  <button className="text-midnightblue transition hover:text-cinnabar">
+                    <span className="sr-only">Remove item</span>
+
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      className="h-4 w-4"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </li>
             </ul>
 
             <div className="mt-8 flex justify-end border-t border-gray-100 pt-8">
